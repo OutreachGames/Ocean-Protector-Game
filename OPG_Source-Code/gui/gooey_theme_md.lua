50685e9f5477414fc8dc175c2e02431f0a529f97
wookieejedi <azthepaleo@gmail.com>
2023-02-28 09:44:37 -0500

    accent gui buttons

--- OPG_Source-Code/gui/gooey_theme_md.lua
+++ OPG_Source-Code/gui/gooey_theme_md.lua
@@ -41,9 +41,12 @@ local SOUND_ID_BUTTON_3 = "sound_controller#sound_button_3"
 
 local VEC3_1 = vmath.vector3(1)
 
-MYG.COLOR_LIGHT_BLUE = vmath.vector4(0.45, 0.84, 1.1, 1)
+-- recall Saturation was set to 200 as accenting effect in paint.net
+MYG.COLOR_LIGHT_BLUE = vmath.vector4(0.80, 0.90, 1.1, 1)
+MYG.COLOR_MEDIUM_BLUE = vmath.vector4(0.70, 0.80, 1.0, 1)
+MYG.COLOR_CONTRAST_BLUE = vmath.vector4(0.55, 0.84, 1.1, 1)
 
-MYG.COLOR_HOVER = MYG.COLOR_LIGHT_BLUE --vmath.vector4(1.15, 1.15, 1.15, 1)
+MYG.COLOR_HOVER = MYG.COLOR_CONTRAST_BLUE --vmath.vector4(1.15, 1.15, 1.15, 1)
 MYG.COLOR_OUTPRESSED = vmath.vector4(1)
 MYG.COLOR_DEFAULT = vmath.vector4(0.90, 0.90, 0.90, 1)
 
