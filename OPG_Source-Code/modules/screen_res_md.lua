30e0b87ee1ff8dca1ef760a28f6d8f8f8b911dbf
wookieejedi <azthepaleo@gmail.com>
2023-05-06 21:36:31 -0400

    fix some strange Z overlaps

--- OPG_Source-Code/modules/screen_res_md.lua
+++ OPG_Source-Code/modules/screen_res_md.lua
@@ -2,7 +2,7 @@
 
 local RES = {}
 
-local internal_game_resolution = {x=1600, y=900} 
+local internal_game_resolution = {x=1600, y=900}
 -- values to this are also used through code where scaling res is used
 -- so if development res changes a lot of things will need to potentially change
 
@@ -15,8 +15,8 @@ RES.Internal_Y = internal_game_resolution.y
 RES.Scaling_X = internal_game_resolution.x*(1/internal_game_resolution.x)
 RES.Scaling_Y = internal_game_resolution.y*(1/internal_game_resolution.y)
 
-RES.Correction_X = internal_game_resolution.x/-2 --set to 0 if using default Defold render
-RES.Correction_Y = internal_game_resolution.y/-2 --set to 0 if using default Defold render
+RES.Correction_X = internal_game_resolution.x/2 --set to 0 if using default Defold render
+RES.Correction_Y = internal_game_resolution.y/2 --set to 0 if using default Defold render
 
 RES.Boundary_X = {internal_game_resolution.x/-2, internal_game_resolution.x/2} --set to 0 if using default Defold render
 RES.Boundary_Y = {internal_game_resolution.y/-2, internal_game_resolution.y/2} --set to 0 if using default Defold render
