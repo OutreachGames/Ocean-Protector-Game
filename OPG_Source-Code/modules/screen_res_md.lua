da2e42b4da72c3854401aecb3dbfe2245ec412a3
wookieejedi <azthepaleo@gmail.com>
2023-05-07 09:27:07 -0400

    some spawn and z fixes

--- OPG_Source-Code/modules/screen_res_md.lua
+++ OPG_Source-Code/modules/screen_res_md.lua
@@ -20,7 +20,7 @@ RES.Correction_Y = internal_game_resolution.y/2 --set to 0 if using default Defo
 -- correction is how to transform current back to Defold default
 -- offset would be inverse sign of correction, and is how to transform Defold default to current
 
-RES.Boundary_X = {internal_game_resolution.x/-2, internal_game_resolution.x/2} --set to 0 if using default Defold render
-RES.Boundary_Y = {internal_game_resolution.y/-2, internal_game_resolution.y/2} --set to 0 if using default Defold render
+RES.Boundary_X = {internal_game_resolution.x/-2, internal_game_resolution.x/2} --set to (0, X_max) if using default Defold render
+RES.Boundary_Y = {internal_game_resolution.y/-2, internal_game_resolution.y/2} --set to (0, Y_max) if using default Defold render
 
 return RES
