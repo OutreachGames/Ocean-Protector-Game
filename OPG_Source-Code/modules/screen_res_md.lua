f4a61789750d3a2c4e15ba12bc6557fd95b74d6b
wookieejedi <azthepaleo@gmail.com>
2023-05-16 16:09:00 -0400

    update boat logic

--- OPG_Source-Code/modules/screen_res_md.lua
+++ OPG_Source-Code/modules/screen_res_md.lua
@@ -23,4 +23,7 @@ RES.Correction_Y = internal_game_resolution.y/2 --set to 0 if using default Defo
 RES.Boundary_X = {internal_game_resolution.x/-2, internal_game_resolution.x/2} --set to (0, X_max) if using default Defold render
 RES.Boundary_Y = {internal_game_resolution.y/-2, internal_game_resolution.y/2} --set to (0, Y_max) if using default Defold render
 
+RES.Center_X = 0 -- set to half of game res min max if using default Defold render
+RES.Center_Y = 0 -- set to half of game res min max if using default Defold render
+
 return RES
