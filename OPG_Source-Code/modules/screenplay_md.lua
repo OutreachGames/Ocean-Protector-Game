981ac7b3a338ceb4f0c0ccb5da6529d58a2ff04a
wookieejedi <azthepaleo@gmail.com>
2023-05-26 10:57:13 -0400

    setup starting scene cleanup

--- OPG_Source-Code/modules/screenplay_md.lua
+++ OPG_Source-Code/modules/screenplay_md.lua
@@ -114,17 +114,22 @@ STR.Screenplay = {
         -- New popup box appears in the top header after each goal is completed. 
 
         user_lesson_02 = {
-            --#TODO add this comment back in
-            --goal_completed_type = STR.CV.goal_completed_types.class_click_items,
+            --#TODO get clicking working
+            goal_completed_type = STR.CV.goal_completed_types.class_click_items,
             goal_text = "Find and document the base of our ocean food-web.",
             display_text = "Identify the base of the food-web in this scene by clicking.",
-            debrief_text = "Plankton "..n.."Plankton are very small organisms that float around the ocean. They are the foundation of ocean food webs. There are two main groups of plankton, phytoplankton, and zooplankton."..ntab.."Phytoplankton are producers that use sunlight to get energy."..ntab.."Zooplankton are consumers that eat other plankton to get energy.",
+            debrief_text = {
+                "",
+                item_plankton = {
+                    "Plankton "..n.."Plankton are very small organisms that float around the ocean. They are the foundation of ocean food webs. There are two main groups of plankton, phytoplankton, and zooplankton."..ntab.."Phytoplankton are producers that use sunlight to get energy."..ntab.."Zooplankton are consumers that eat other plankton to get energy."
+                }
+            },
             extra_text = ""
         },
 
         user_lesson_03 = {
-            --#TODO add this comment back in
-            --goal_completed_type = STR.CV.goal_completed_types.class_click_items,
+            --#TODO get clicking working
+            goal_completed_type = STR.CV.goal_completed_types.class_click_items,
             goal_text = "Find and document consumers in our ocean food-web.",
             display_text = "Identify another component of the food-web in this scene by clicking.",
             debrief_text = {
@@ -165,11 +170,16 @@ STR.Screenplay = {
         -- Boat slides onto screen.
 
         user_lesson_05 = {
-            --#TODO add this comment back in
-            --goal_completed_type = STR.CV.goal_completed_types.class_click_items,
+            --#TODO get clicking working
+            goal_completed_type = STR.CV.goal_completed_types.class_click_items,
             goal_text = "Find and document the highest-level consumer in our ocean food-web.",
             display_text = "Identify the highest-level component in the ocean food-web by clicking.",
-            debrief_text = "Humans"..n.."Though humans do not live in the water we rely heavily on our oceans!",
+            debrief_text = {
+                "",
+                item_humans = {
+                    "Humans"..n.."Though humans do not live in the water we rely heavily on our oceans!"
+                }
+            },
             extra_text = ""
         },
 
@@ -284,8 +294,8 @@ STR.Screenplay = {
         -- Also show text explanations listed below. 
 
         user_lesson_18 = {
-            --#TODO add this comment back in
-            --goal_completed_type = STR.CV.goal_completed_types.class_click_items,
+            --#TODO get clicking working
+            goal_completed_type = STR.CV.goal_completed_types.class_click_items,
             goal_text = "Examine how each animal group has changed under more acidic conditions in our ocean scene.",
             display_text = "Identify each component of the food-web has changed under more acidic oceans by clicking.",
             debrief_text = {
