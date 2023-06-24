1489d2cf5335c1b70261a0dc63324228b2147184
wookieejedi <azthepaleo@gmail.com>
2023-06-24 15:12:25 -0400

    cleanup on animation rates

--- OPG_Source-Code/modules/item_info_md.lua
+++ OPG_Source-Code/modules/item_info_md.lua
@@ -313,7 +313,7 @@ INFO.item_info = {
                 object_translation_speed_base = 3,
                 object_translation_speed_can_vary = true,
                 object_logic_type = CV_Logic_Type_Swimmer,
-                object_anim_multi = {idle = 0.4, moving = 0.8},
+                object_anim_multi = {rate_idle = 0.4, rate_moving = 0.8, rate_float = 0.4},
                 movement_easing_options = {move_go=5, move_stop=5},
                 info_url = "https://www.fisheries.noaa.gov/species/queen-conch"
             }
@@ -344,7 +344,7 @@ INFO.item_info = {
                 object_translation_speed_base = 40,
                 object_logic_type = CV_Logic_Type_Swimmer,
                 object_death_type = 1,
-                object_anim_multi = {idle = 0.25, moving = 1.5, extra = 0.5},
+                object_anim_multi = {rate_idle = 1.0, rate_moving = 1.5, rate_float = 0.25, rate_extra = 0.5},
                 movement_easing_options = {move_go=1, move_stop=2},
                 info_url = "https://www.fisheries.noaa.gov/species/western-atlantic-bluefin-tuna"
             },
@@ -362,7 +362,7 @@ INFO.item_info = {
                 object_translation_speed_base = 30,
                 object_logic_type = CV_Logic_Type_Swimmer,
                 object_death_type = 1,
-                object_anim_multi = {idle = 0.25, moving = 1.5, extra = 0.8},
+                object_anim_multi = {rate_idle = 1.0, rate_moving = 1.5, rate_float = 0.25, rate_extra = 0.8},
                 movement_easing_options = {move_go=1, move_stop=2},
                 info_url = "https://www.fisheries.noaa.gov/species/bluefish"
             },
@@ -380,7 +380,7 @@ INFO.item_info = {
                 object_translation_speed_base = 20,
                 object_logic_type = CV_Logic_Type_Swimmer,
                 object_death_type = 1,
-                object_anim_multi = {idle = 0.25, moving = 1.5, extra = 0.7},
+                object_anim_multi = {rate_idle = 1.0, rate_moving = 1.5, rate_float = 0.25, rate_extra = 0.7},
                 movement_easing_options = {move_go=1, move_stop=2},
                 info_url = "https://www.fisheries.noaa.gov/species/red-snapper"
             }
@@ -413,7 +413,7 @@ INFO.item_info = {
                 object_translation_speed_base = 8,
                 object_translation_speed_can_vary = true,
                 object_logic_type = CV_Logic_Type_Swimmer,
-                object_anim_multi = {idle = 0.6, moving = 1.0, extra = 0.6}, --recall idle and extra are just claws
+                object_anim_multi = {rate_idle = 0.001, rate_moving = 1.0, rate_float = 0.6, rate_extra = 0.6}, --recall extra and float are just claws
                 movement_easing_options = {move_go=5, move_stop=5},
                 info_url = "https://www.fisheries.noaa.gov/species/blue-crab"
             },
@@ -431,7 +431,7 @@ INFO.item_info = {
                 object_rotation_speed_base = 40,
                 object_translation_speed_base = 15,
                 object_logic_type = CV_Logic_Type_Swimmer,
-                object_anim_multi = {idle = 0.5, moving = 1.2}, --recall idle is moving with current
+                object_anim_multi = {rate_idle = 0.5, rate_moving = 1.2, rate_float = 0.5}, --recall idle is moving with current
                 movement_easing_options = {move_go=2, move_stop=2},
                 info_url = "https://www.fisheries.noaa.gov/species/pink-shrimp"
             }
